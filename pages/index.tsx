25023fabd37d1873fea6aa542e709718d677b78c
Mevlut Canvar <mevlutcanvar@gmail.com>
2022-05-05 17:38:19 +0300

    getting ready for prod

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import {Spring, animated, easings} from 'react-spring'
 import {useState} from "react";
+import Link from "next/link";
 
 const Home: NextPage = () => {
 
@@ -17,7 +18,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Toon<a href="https://nextjs.org">verse</a>
+          Toon<Link href="https://nextjs.org">verse</Link>
         </h1>
 
         <p className={styles.description}>
@@ -25,54 +26,49 @@ const Home: NextPage = () => {
         </p>
 
         <div className={styles.grid}>
-            <Spring
-                loop={{reverse: true}}
-                from={{ opacity: 1, rotateY: 0 }}
-                to={[
-                    { opacity: 0.7, rotateY: 25 },
-                    { opacity: 1, rotateY: 0 },
-                ]}
-                config={{duration: 2000, easing: easings.easeInOutQuart}}
-            >
-                {s => (
-                    <animated.div style={s}>
-                        <a href={'https://twitter.com/ToonverseClub'} className={styles.card}>
-                            <Image src={'/logo-twitter.svg'} width={100} height={100} />
-                        </a>
-                    </animated.div>
-                )}
-            </Spring>
-            <Spring
-                loop={{reverse: true}}
-                from={{ opacity: 0.7, rotateY: 25 }}
-                to={[
-                    { opacity: 1, rotateY: 0 },
-                    { opacity: 0.7, rotateY: 25 },
-                ]}
-                config={{duration: 2000, easing: easings.easeInOutQuart}}
-            >
-                {s => (
-                    <animated.div style={s}>
-                        <a href={'https://discord.gg/CAb8rcJB'} className={styles.card}>
-                            <Image src={'/logo-discord.svg'} width={100} height={100} />
-                        </a>
-                    </animated.div>
-                )}
-            </Spring>
-
-
+            <div className={styles.card}>
+                <Spring
+                    loop={{reverse: true}}
+                    from={{ opacity: 1, rotateY: 0 }}
+                    to={[
+                        { opacity: 0.7, rotateY: 25 },
+                        { opacity: 1, rotateY: 0 },
+                    ]}
+                    config={{duration: 2000, easing: easings.easeInOutQuart}}
+                >
+                    {s => (
+                        <animated.div style={s}>
+                            <Link href={'https://twitter.com/ToonverseClub'}>
+                                <Image alt={'Twitter'} src={'/logo-twitter.svg'} width={100} height={100} />
+                            </Link>
+                        </animated.div>
+                    )}
+                </Spring>
+            </div>
+            <div className={styles.card}>
+                <Spring
+                    loop={{reverse: true}}
+                    from={{ opacity: 0.7, rotateY: 25 }}
+                    to={[
+                        { opacity: 1, rotateY: 0 },
+                        { opacity: 0.7, rotateY: 25 },
+                    ]}
+                    config={{duration: 2000, easing: easings.easeInOutQuart}}
+                >
+                    {s => (
+                        <animated.div style={s}>
+                            <Link href={'https://discord.gg/CAb8rcJB'}>
+                                <Image alt={'Discord'} src={'/logo-discord.svg'} width={100} height={100} />
+                            </Link>
+                        </animated.div>
+                    )}
+                </Spring>
+            </div>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="/"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-
-            <h5 className={styles.title}>Toon<span>verse</span></h5>
-        </a>
+          Powered by{' '}<h5 className={styles.title}>Toon<span>verse</span></h5>
       </footer>
     </div>
   )
