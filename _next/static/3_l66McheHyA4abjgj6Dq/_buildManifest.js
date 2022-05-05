cfc8d6fae335439bfeb901f9bca0428acc7d9dce
mcanvar <mcanvar@users.noreply.github.com>
2022-05-05 15:05:12 +0000

    deploy: 0ca49499230e68cb09aafcaeca9c3a496e2aa176

---
+++ _next/static/3_l66McheHyA4abjgj6Dq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/949-bae72141053fefb2.js","static/css/bc2eecc6e8d418df.css","static/chunks/pages/index-f80c6d2c101d031e.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/TAJ6fuYQGaCjdGakJX1X_/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/949-bae72141053fefb2.js","static/css/bc2eecc6e8d418df.css","static/chunks/pages/index-c0c6f4f7573d88ba.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
