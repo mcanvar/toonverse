43c1c10fec621d8b5a37bcf77396bef6cb5daca4
mcanvar <mcanvar@users.noreply.github.com>
2022-05-06 11:13:08 +0000

    deploy: b96568d1425f20a1580dc44bbd805f9bdf55e026

---
+++ _next/static/dc33DDuUAJypwaMRrlwCY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/949-bae72141053fefb2.js","static/css/bc2eecc6e8d418df.css","static/chunks/pages/index-b6564218fba56914.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
